eb5345e0006c55952446eb3c37ecc2d051d10ed0
418808imai <96104652+418808imai@users.noreply.github.com>
2021-12-21 10:20:13 +0900

    Add files via upload

---
+++ design_1_wrapper_hw_platform_0/drivers/muladd_v1_0/data/muladd.tcl
@@ -0,0 +1,21 @@
+# ==============================================================
+# Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2019.1_AR72614 (64-bit)
+# Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+# ==============================================================
+proc generate {drv_handle} {
+    xdefine_include_file $drv_handle "xparameters.h" "XMuladd" \
+        "NUM_INSTANCES" \
+        "DEVICE_ID" \
+        "C_S_AXI_AXILITES_BASEADDR" \
+        "C_S_AXI_AXILITES_HIGHADDR"
+
+    xdefine_config_file $drv_handle "xmuladd_g.c" "XMuladd" \
+        "DEVICE_ID" \
+        "C_S_AXI_AXILITES_BASEADDR"
+
+    xdefine_canonical_xpars $drv_handle "xparameters.h" "XMuladd" \
+        "DEVICE_ID" \
+        "C_S_AXI_AXILITES_BASEADDR" \
+        "C_S_AXI_AXILITES_HIGHADDR"
+}
+
